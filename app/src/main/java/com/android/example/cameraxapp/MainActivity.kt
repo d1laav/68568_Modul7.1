3ba6b50dc4136404599dad6a2f389a84b546e30a
d1laav <dimmanuel31@gmail.com>
2025-03-18 21:07:44 +0700

    3. Request the necessary permissions

--- app/src/main/java/com/android/example/cameraxapp/MainActivity.kt
+++ app/src/main/java/com/android/example/cameraxapp/MainActivity.kt
@@ -5,8 +5,10 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 
 import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.ImageCapture
 import androidx.camera.video.VideoCapture
 import androidx.camera.video.Recorder
@@ -29,6 +31,27 @@ class MainActivity : ComponentActivity() {
     private var recording: Recording? = null
 
     private  lateinit var cameraExecutor: ExecutorService
+
+    private val activityResultLauncher =
+        registerForActivityResult(
+                ActivityResultContracts.RequestMultiplePermissions())
+        {
+            permissions ->
+            var permissionGranted = true
+            permissions.entries.forEach {
+                if (it.key in REQUIRED_PERMISSIONS && it.value == false)
+                    permissionGranted = false
+            }
+            if (!permissionGranted) {
+                Toast.makeText(baseContext,
+                    "Permission request denied",
+                    Toast.LENGTH_SHORT).show()
+            } else {
+                startCamera()
+            }
+        }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
@@ -53,7 +76,9 @@ class MainActivity : ComponentActivity() {
 
     private fun startCamera() {}
 
-    private fun requestPermissions() {}
+    private fun requestPermissions() {
+        activityResultLauncher.launch(REQUIRED_PERMISSIONS)
+    }
 
     private fun allPermissionGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
